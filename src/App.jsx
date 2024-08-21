f925671e3ab4aba622e71cb8d5420b6a9efbf978
Tofiq311 <ashik.tofiq.1@gmail.com>
2024-08-21 18:43:25 +0900

    deploy react to github

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import Navbar from "./Components/navbar"
+import NewsBoard from "./Components/NewsBoard"
+
+
+const App = () => {
+
+const [category,setCategory] = useState("general");
+
+  return (
+    <div>
+      
+    <Navbar setCategory={setCategory}/>
+    <NewsBoard category={category}/>
+      
+    </div>
+  )
+}
+
+export default App
